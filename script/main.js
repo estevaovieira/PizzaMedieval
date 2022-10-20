624c7ab97170bbc5fde3b24175f4b356ec9b1bc9
estevaovieira <estevaocisneros@yahoo.com.br>
2022-10-19 21:36:46 -0300

    git commit -m "atualização do cardapio para Js"

--- script/main.js
+++ script/main.js
@@ -1,10 +1,72 @@
-let pizza = []
-const endPointDaApi = 'https://raw.githubusercontent.com/estevaovieira/PizzaMedieval/develop/data/requisicaoMassas.json'
-getBuscarpizzasDaApi()
-const elementoParaCadaPizza = document.getElementById('pizza')
+let cardPizza = []
+let cardMassa = []
 
-async function getBuscarpizzasDaApi() {
+const endPointDaApi = 'https://raw.githubusercontent.com/estevaovieira/PizzaMedieval/develop/data/requisicaoPizza.json'
+getBuscarPizzasDaApi()
+const elementoParaCadaPizza = document.getElementById('pizza')
+const ul = document.getElementById('pizza')
+
+const endPointDaApi2 = 'https://raw.githubusercontent.com/estevaovieira/PizzaMedieval/develop/data/requisicaoMassas.json'
+getBuscarMassaDaApi()
+const elementoParaCadaMassa = document.getElementById('massa')
+const ul2 = document.getElementById('massa')
+
+async function getBuscarPizzasDaApi() {
     const res = await fetch(endPointDaApi)
-    pizza = await res.json()
-    console.table(pizza)
+    cardPizza = await res.json()
+    exibirPizzasNaTela(cardPizza)
+}
+
+async function getBuscarMassaDaApi() {
+    const res2 = await fetch(endPointDaApi2)
+    cardMassa = await res2.json()
+    exibirMassasNaTela(cardMassa)
+}
+
+function exibirPizzasNaTela(listaDePizzas) {
+    listaDePizzas.forEach(cardPizzas => {
+        creatcard(cardPizzas)
+    });
+
+    function creatcard(cardPizzas) {
+        let li = document.createElement('li')
+        let titulo = document.createElement('h3')
+        let pizzaImg = document.createElement('img')
+        let description = document.createElement('p')
+        let preco = document.createElement('p')
+
+        titulo.textContent = cardPizzas.titulo;
+        pizzaImg.src = cardPizzas.imagem;
+        description.textContent = cardPizzas.descricao;
+        preco.textContent = cardPizzas.preco;
+
+        li.append(titulo, pizzaImg, description, preco)
+        description.className = 'descricao'
+        preco.className = 'preco'
+        ul.appendChild(li)
+    }
+}
+
+function exibirMassasNaTela(listaDeMassas) {
+    listaDeMassas.forEach(cardMassas => {
+        creatcard(cardMassas)
+    });
+
+    function creatcard(cardMassas) {
+        let li = document.createElement('li')
+        let titulo = document.createElement('h3')
+        let pizzaImg = document.createElement('img')
+        let description = document.createElement('p')
+        let preco = document.createElement('p')
+
+        titulo.textContent = cardMassas.titulo;
+        pizzaImg.src = cardMassas.imagem;
+        description.textContent = cardMassas.descricao;
+        preco.textContent = cardMassas.preco;
+
+        li.append(titulo, pizzaImg, description, preco)
+        description.className = 'descricao'
+        preco.className = 'preco'
+        ul2.appendChild(li)
+    }
 }
